869016264138f68efe87eb7f20b22b3f582a3679
CortexVacua <45538598+CortexVacua@users.noreply.github.com>
2020-03-06 10:47:28 +0100

    Update User.java

--- src/main/java/ch/uzh/ifi/seal/soprafs20/entity/User.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/entity/User.java
@@ -11,7 +11,7 @@ import java.util.Date;
  * Internal User Representation
  * This class composes the internal representation of the user and defines how the user is stored in the database.
  * Every variable will be mapped into a database field with the @Column annotation
- * - nullable = false -> this cannot be left empty
+ *  nullable = false -> this cannot be left empty
  * - unique = true -> this value must be unqiue across the database -> composes the primary key
  */
 @Entity
